b4a8a862731af9c579940d62011e7598c518b6af
misakano7545 <168344685+misakano7545@users.noreply.github.com>
2024-11-03 16:35:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ install_V2bX() {
     if [[ $first_install == true ]]; then
         read -rp "检测到你为第一次安装V2bX,是否自动直接生成配置文件？(y/n): " if_generate
         if [[ $if_generate == [Yy] ]]; then
-            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/InazumaV/V2bX-script/master/initconfig.sh
+            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/misakano7545/v2bx-script/refs/heads/main/initconfig.sh
             source initconfig.sh
             rm initconfig.sh -f
             generate_config_file
